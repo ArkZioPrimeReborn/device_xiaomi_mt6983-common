616f88429ac5c79be5bfa4b8919172f535578d6b
ArkzioPrimeReborn <xigaaa12138@gmail.com>
2025-11-25 22:15:55 +0800

    mt6983-common: Change the correct setting options

--- extract-files.py
+++ extract-files.py
@@ -34,12 +34,12 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
-    ('vendor/lib64/mt6895/libaalservice.so', 'vendor/bin/mnld'): blob_fixup()
+    ('vendor/lib64/mt6983/libaalservice.so', 'vendor/bin/mnld'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
-    'vendor/lib64/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+    'vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
-    'vendor/lib64/mt6895/libmnl.so': blob_fixup()
+    'vendor/lib64/mt6983/libmnl.so': blob_fixup()
         .add_needed('libcutils.so'),
     ('vendor/lib64/libnvram.so','vendor/lib64/libsysenv.so'): blob_fixup()
     	.add_needed('libbase_shim.so'),
